8edeb9cadf2440686a547783e3232e735694f15b
DeAnna Boiani <deannaboiani@gmail.com>
2021-12-06 16:51:48 -0800

    bug fix for store

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-// import { useStoreContext } from '../../utils/GlobalState';
 import { useDispatch, useSelector } from 'react-redux'
 
 import {
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Cart from '../components/Cart';
-// import { useStoreContext } from '../utils/GlobalState';
 import { useDispatch, useSelector } from 'react-redux'
 import {
   REMOVE_FROM_CART,
